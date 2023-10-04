a54c2593317222ae45329474f74670332edb2c99
Penguinify <103973145+Penguinify@users.noreply.github.com>
2023-10-04 14:03:48 -0700

    added some images

--- src/modules/modules/games/gamesPackager.ts
+++ src/modules/modules/games/gamesPackager.ts
@@ -1,9 +1,55 @@
 import { moduleDefinition, change_game, none } from '../../moduleapi'
+import { UIManager } from '../../../UIManager'
 import gamesJSON from './games.json'
+import { create_element } from '../../../UILib'
+import "./games.ts.less"
 
 type game = {
     name: string
     url: string
+    imgurl?: string
+}
+
+function render(UI: UIManager, title: string, url: string, imgurl: string) {
+    const games_section = UI.getSectionFromID("game")
+    
+    if (!games_section) return
+
+    let games_container = create_element(
+        "div",
+        games_section.section_content,
+        {
+            "class_name": "cac__game__button"
+        }
+    )
+
+    games_container.addEventListener("onclick", (e) => {
+        change_game(url)
+    })
+
+    let image_element = create_element(
+        "img",
+        games_container,
+        {
+            "class_name": "cac__game__image",
+        }
+    )
+
+    image_element.setAttribute(
+        "src",
+        imgurl
+    )
+
+    let title_element = create_element(
+        "p",
+        games_container,
+        {
+            "class_name": "cac__game__title",
+            "innerHTML": title
+        }
+    )
+
+
 }
 
 // maps them
@@ -11,13 +57,8 @@ const mapped_games: moduleDefinition[] = []
 
 gamesJSON.forEach((game: game) => {
     let mapped_game: moduleDefinition = {
-        display_name: game['name'],
-        id: game['name'],
-        onactive: () => change_game(game['url']),
-        ondisable: none,
-        section: 'game',
-        reset: true,
-        custom_render: false,
+        custom_render: true,
+        render: (UI: UIManager) => render(UI, game.name, game.url, game.imgurl || "https://placehold.co/600x400?text=game+image+ig")
     }
 
     mapped_games.push(mapped_game)
