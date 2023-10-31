5f1d174d7cfd08067c9511811556344e90369aed
Penguinify <103973145+Penguinify@users.noreply.github.com>
2023-10-30 19:47:44 -0700

    made a history flooder (w)

---
+++ src/modules/modules/fun/historyflooder.ts
@@ -0,0 +1,23 @@
+import { moduleDefinition } from "../../moduleapi";
+
+function historyFlooder() {
+    let i = 0;
+    while (i < 100) {
+        history.pushState(0,"", i.toString());
+        i++;
+    }
+
+    history.pushState(0, "", window.location.href);
+}
+
+const plugin: moduleDefinition = {
+    custom_render: false,
+    display_name: "History Flooder",
+    description: "Floods your history with current page",
+    id: "historyflooder",
+    section: "fun",
+    reset: true,
+    onactive: historyFlooder
+}
+
+export default plugin;
