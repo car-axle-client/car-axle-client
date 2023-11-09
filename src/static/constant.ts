8ff739b6b6db464b81192c6a53e599119d2e29dc
Penguinify <103973145+Penguinify@users.noreply.github.com>
2023-11-08 18:00:20 -0800

    new colors

--- src/static/constant.ts
+++ src/static/constant.ts
@@ -1,8 +1,8 @@
 export const GAMESLINK = 'https://penguinify-web-dev.github.io/'
-export const VERSION = '5'
+export const VERSION = '6'
 export const ITERATION = '0'
 export const CLIENTNAME = 'car axle client'
-export const AUTHOR = 'penguinify (early christmas)'
+export const AUTHOR = 'penguinify (merry christmas)'
 export const LOGO =
     'https://delivery.contenthub.allstate.com/api/public/content/f0e5db4104d04bf386f97f6dc098bfc5?v=353a1ed1'
 export const SOCIAL = {
