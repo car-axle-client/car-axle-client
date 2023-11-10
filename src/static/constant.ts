ff19a783ccb4f19212b65112a957661fbbbfdbd8
Penguinify <103973145+Penguinify@users.noreply.github.com>
2023-11-09 18:29:24 -0800

    new games
    Co-Authored-By: Penguin-dev-web <69807318+Penguin-dev-web@users.noreply.github.com>

--- src/static/constant.ts
+++ src/static/constant.ts
@@ -2,9 +2,9 @@ export const GAMESLINK = 'https://penguinify-web-dev.github.io/'
 export const VERSION = '6'
 export const ITERATION = '0'
 export const CLIENTNAME = 'car axle [beta]'
-export const AUTHOR = 'penguinify (merry christmas)'
+export const AUTHOR = 'penguinify (cherry)'
 export const LOGO =
-    'https://delivery.contenthub.allstate.com/api/public/content/f0e5db4104d04bf386f97f6dc098bfc5?v=353a1ed1'
+    'https://c.wallhere.com/photos/24/40/minimalism_red_abstract_digital_art_artwork_low_poly_geometry-158455.png!d'
 export const SOCIAL = {
     github: 'https://github.com/car-axle-client/car-axle-client',
     discord: 'https://discord.gg/nac46r6Qn7',
