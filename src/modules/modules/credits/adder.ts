bd2f4117d069722a1c83fd4bf3984b4d185c4131
Penguinify <103973145+Penguinify@users.noreply.github.com>
2023-11-09 22:06:13 -0800

    credits

---
+++ src/modules/modules/credits/adder.ts
@@ -0,0 +1,15 @@
+
+
+import { moduleDefinition } from '../../moduleapi'
+
+const plugin: moduleDefinition = {
+    display_name: 'ading2210 for Edupuzzle Answers',
+    description:
+        'thanks',
+    id: 'cred',
+    section: 'credit',
+    disabled: true,
+    custom_render: false,
+}
+
+export default plugin
