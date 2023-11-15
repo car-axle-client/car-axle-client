9516fca6b06a2eb052597a97870b2fd39f7e6815
Penguinify <103973145+Penguinify@users.noreply.github.com>
2023-11-14 17:28:07 -0800

    new logo and some other stuff

--- src/static/constant.ts
+++ src/static/constant.ts
@@ -4,7 +4,7 @@ export const ITERATION = '0'
 export const CLIENTNAME = 'car axle [beta]'
 export const AUTHOR = 'penguinify (cherry)'
 export const LOGO =
-    'https://th.bing.com/th/id/OIP.fyPGSbxqwcg15C2uZ7b5WwHaEK?pid=ImgDet&rs=1'
+    'https://github.com/car-axle-client/car-axle-client/tree/main/docs/logo.png'
 export const SOCIAL = {
     github: 'https://github.com/car-axle-client/car-axle-client',
     discord: 'https://discord.gg/akDMdW7Zgd',
