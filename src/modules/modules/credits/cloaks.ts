eaa1ee7ea24540bcd931dfade11360a742f1cfc1
Penguinify <103973145+Penguinify@users.noreply.github.com>
2023-12-03 12:16:50 -0800

    added selenite credits

---
+++ src/modules/modules/credits/cloaks.ts
@@ -0,0 +1,12 @@
+import { moduleDefinition } from '../../moduleapi'
+
+const plugin: moduleDefinition = {
+    display_name: 'selenite',
+    description: 'for their cloaks list that I blatantly stole',
+    id: 'cred',
+    section: 'credit',
+    disabled: true,
+    custom_render: false,
+}
+
+export default plugin
