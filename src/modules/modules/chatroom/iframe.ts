b067b5a92549e9d1fc1bb3f84df513f8820694cf
Penguinify <103973145+Penguinify@users.noreply.github.com>
2023-12-07 16:54:39 -0800

    new chatroom

--- src/modules/modules/chatroom/iframe.ts
+++ src/modules/modules/chatroom/iframe.ts
@@ -1,12 +1,27 @@
 import { moduleDefinition, new_iframe } from '../../moduleapi'
 import '../../../static/iframe.less'
 import { UIManager } from '../../../UIManager'
+import { create_element } from '../../../UILib'
 
 function render(UI: UIManager) {
     let section_content = UI.getSectionFromID('chat')?.section_content
     if (!section_content) return
 
-    let iframe = new_iframe(UI, section_content, atob('aHR0cHM6Ly9vcmdhbml6YXRpb25zLm1pbm5pdC5jaGF0LzcxMzIyNjQ1MTIxMTIzNy9jL2dlbmVyYWw/ZW1iZWQmbmlja25hbWU9'))
+    // Assuming section_content is a DOM element
+    const widgetbotElement = document.createElement('widgetbot');
+    widgetbotElement.setAttribute('server', '1143730031533768726');
+    widgetbotElement.setAttribute('channel', '1182483663905103882');
+    widgetbotElement.setAttribute('width', '800');
+    widgetbotElement.setAttribute('height', '600');
+
+    // Create script element
+    const scriptElement = document.createElement('script');
+    scriptElement.src = 'https://cdn.jsdelivr.net/npm/@widgetbot/html-embed';
+
+    // Append both elements to section_content
+    section_content.appendChild(widgetbotElement);
+    section_content.appendChild(scriptElement);
+    
 }
 
 const plugin: moduleDefinition = {
