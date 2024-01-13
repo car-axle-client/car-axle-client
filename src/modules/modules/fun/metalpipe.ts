a5f08b6b13805e432a05dee59bd68903bc87e6b4
skrillonrfo <147339739+skrillonrfo@users.noreply.github.com>
2024-01-12 23:03:18 -0600

    fix  src

--- src/modules/modules/fun/metalpipe.ts
+++ src/modules/modules/fun/metalpipe.ts
@@ -1,19 +1,19 @@
 import { moduleDefinition } from '../../moduleapi'
-
+// its metal pipe 'cause i dont got time to change allat (i have no clue what any of it means)
 function metalpipe() {
     let images = document.getElementsByTagName('img')
     if (!images) return
 
     for (let image of images) {
-        var audio = new Audio('https://www.myinstants.com/media/sounds/jixaw-metal-pipe-falling-sound.mp3')
+        var audio = new Audio('https://us-tuna-sounds-files.voicemod.net/2ff3560b-a76f-49dd-adbc-709b9f59c5c2-1704237226346.mp3')
         audio.play()
-        image.src = 'https://i.ytimg.com/vi/qDvMkxtLV74/maxresdefault.jpg'
+        image.src = 'https://us-tuna-sounds-images.voicemod.net/2ff3560b-a76f-49dd-adbc-709b9f59c5c2-1704237226346.jpeg'
     }
 }
 
 const plugin: moduleDefinition = {
-    display_name: 'METAL PIPE',
-    description: 'idk why i added this',
+    display_name: '🎡',
+    description: '🎡',
     id: 'metalpipe',
     section: 'fun',
     onactive: metalpipe,
