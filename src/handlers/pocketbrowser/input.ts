f9e9a5f8b825b77fd75ff6eacfbb777022d3effd
penguinify <penguinifyminecraft@gmail.com>
2024-02-13 18:30:26 -0800

    google.com goes to google

--- src/handlers/pocketbrowser/input.ts
+++ src/handlers/pocketbrowser/input.ts
@@ -1,17 +1,18 @@
 import { Pen } from '../../penexutils'
 import { HandlerDefinition } from '../../types'
 
-async function checkIfIframeIsEmbedable(url: string): Promise<boolean> {
-    return true
-}
-
 function formatInput(input: string): string {
+    input = input.trim().toLowerCase()
     if (!input) {
         input = 'https://google.com/webhp?igu=1'
     } else if (!input.includes('.')) {
         input = `https://google.com/search?q=${input}&igu=1`
     } else if (!input.includes('//')) {
         input = 'https://' + input
+    }
+
+    if (input === 'https://google.com') {
+        input = 'https://google.com/webhp?igu=1'
     }
 
     return input
@@ -21,14 +22,6 @@ function inputhandler(input: string): void {
     input = formatInput(input)
     let iframe = document.getElementById('pocketbrowseriframe') as HTMLIFrameElement
     iframe.setAttribute('src', input)
-
-    checkIfIframeIsEmbedable(input).then((state) => {
-        if (state) {
-            console.log('Iframe loaded')
-        } else {
-            iframe.setAttribute('src', `https://placehold.co/${iframe.clientWidth}x${iframe.clientHeight}?text=Cannot+load+this+website,+try+loading+a+different+website.`)
-        }
-    })
 }
 let definition: HandlerDefinition = {
     type: 'input',
